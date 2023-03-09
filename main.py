e5e548ae3b0869d76e87deae7252922aaae769d0
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-03-09 09:41:02 -0400

    Put the program into a loop, to make it slightly more interactive

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 # Basic  Structure of Program:
 import Utils
 
-# Step 1, Run the program
+# Step 1, Run the program and LOOP!
 
-# Step 2, record x seconds worth of audio
-rc = Utils.Recorder(5)
+text = ""
 
-frames = rc.record()
+while ("goodbye" not in text.lower()):
+    # Step 2, record x seconds worth of audio
+    rc = Utils.Recorder(5)
 
-# # Step 3, save the audio to a file
-rc.save_frames(frames)
+    frames = rc.record()
 
-# Step 4, transcribe the audio from the file to text
-text = Utils.transcribe()
-print("I heard: ", text)
+    # # Step 3, save the audio to a file
+    rc.save_frames(frames)
 
-# Step 5, pass the text to the AI
-# Step 6, let the magic happen
-# Step 7, save the response
-response = Utils.prompt(text)
+    # Step 4, transcribe the audio from the file to text
+    text = Utils.transcribe()
+    print("I heard: ", text)
 
-# Step 8, Read out the response.
+    # Step 5, pass the text to the AI if there is text
+    if not text: continue
+    # Step 6, let the magic happen
+    # Step 7, save the response
+    response = Utils.prompt(text)
 
-print(response)
-Utils.tts(response)
+    # Step 8, Read out the response.
+
+    print(response)
+    Utils.tts(response)
