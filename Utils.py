1a4b657747e5a092680ea2758c7b61fe2135cfdd
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-03-09 00:31:46 -0400

    Added in text to speech and cleaned up code a bit

--- Utils.py
+++ Utils.py
@@ -1,6 +1,6 @@
 # This is the utility file, where all of the "magic" will occur.
 
-import pyaudio, wave
+import pyaudio, wave, pyttsx3
 import openai, os
 
 from dotenv import load_dotenv
@@ -22,7 +22,7 @@ class Recorder:
     
 
     # Initilalize the number of seconds to record
-    def __init__(self, record_seconds=5):
+    def __init__(self, record_seconds=10):
         self.record_seconds = record_seconds
         self.filename = filename
 
@@ -30,11 +30,12 @@ class Recorder:
         """This function will record record_seconds number of seconds worth of audio"""
         audio = pyaudio.PyAudio()
         stream = audio.open(format=pyaudio.paInt16, channels=self.channels, rate=self.sample_rate, input=True, frames_per_buffer=self.chunk)
-        print("I'm listening")
-
+        
         frames = []
 
-        for _ in range(0, int(self.sample_rate / self.chunk * self.record_seconds)):
+        for i in range(0, int(self.sample_rate / self.chunk * self.record_seconds)):
+            if (not i): print("I'm listening")
+
             data = stream.read(self.chunk)
             frames.append(data)
         
@@ -56,14 +57,34 @@ class Recorder:
 
 # This method is used to transcribe information from the specified filename
 
-def transcribe():
+def transcribe() -> str:
+    """Transcribes the audio file into text"""
     with open(filename, 'rb') as fp:
         transcript = openai.Audio.transcribe("whisper-1", fp)
         return transcript['text']
 
 
-def prompt(text:str):
-    p = "You are role-playing as an amazing assistant to help someone who is often confused. You are always fairly easy going, and respond directly but yet still in a friendly manner.\n" + text
-    response = openai.Completion.create(model="text-davinci-003", prompt=p, temperature=1, max_tokens=100)
+def prompt(text:str) -> str:
+    """Prompts the text-davinci-003 model for a response given the recorded text"""
+    p = "You are role-playing as an amazing assistant named Timmy. Your role is to help someone who is often confused. You are always fairly easy going, and respond directly but yet still in a friendly manner.\n" + text
+    response = openai.Completion.create(model="text-davinci-003", prompt=p, temperature=1, max_tokens=1000)
 
     return response["choices"][0]["text"]
+
+
+def tts(text:str):
+    """Speaks out the given speech"""
+    engine = pyttsx3.init()
+
+    # Set the voice
+    engine.setProperty('voice', "./voice.flitevox")
+
+    # Set the rate
+    engine.setProperty('rate', 170) # you can adjust this value as needed
+
+    # Set the volume
+    engine.setProperty('volume', 1.0) # you can adjust this value as needed
+
+    # Say something
+    engine.say(text)
+    engine.runAndWait()
--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import Utils
 
 # Step 1, Run the program
 
-# Step 2, record 10 seconds worth of audio
-# rc = Utils.Recorder()
+# Step 2, record x seconds worth of audio
+rc = Utils.Recorder(3)
 
-# frames = rc.record()
+frames = rc.record()
 
 # # Step 3, save the audio to a file
-# rc.save_frames(frames)
+rc.save_frames(frames)
 
 # Step 4, transcribe the audio from the file to text
 text = Utils.transcribe()
+print("I heard: ", text)
 
 # Step 5, pass the text to the AI
-
+# Step 6, let the magic happen
+# Step 7, save the response
 response = Utils.prompt(text)
 
-# Step 6, let the magic happen
-
-# Step 7, save the response
-
 # Step 8, Read out the response.
+
+print(response)
+Utils.tts(response)
