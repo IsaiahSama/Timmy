4d13d4ad9c21256b1fdfcae6d890527c7aa1eba0
Isaiah-Sama <zeldaplayergl@gmail.com>
2023-03-15 21:14:44 -0400

    Switched models and fixed issues with context

--- Utils.py
+++ Utils.py
@@ -4,6 +4,7 @@ import pyaudio, wave, pyttsx3
 import openai, os
 
 from dotenv import load_dotenv
+from keyboard import is_pressed
 
 load_dotenv()
 
@@ -19,6 +20,7 @@ class Recorder:
     channels = 2
     sample_rate = 44100
     record_seconds = 0
+    recording = False
     
 
     # Initilalize the number of seconds to record
@@ -32,16 +34,18 @@ class Recorder:
         stream = audio.open(format=pyaudio.paInt16, channels=self.channels, rate=self.sample_rate, input=True, frames_per_buffer=self.chunk)
         
         frames = []
+        print("I'm listening")
 
-        for i in range(0, int(self.sample_rate / self.chunk * self.record_seconds)):
-            if (not i): print("I'm listening")
-
+        # for i in range(0, int(self.sample_rate / self.chunk * self.record_seconds)):
+        while not (is_pressed("c")):
             data = stream.read(self.chunk)
             frames.append(data)
         
+        print("Thinking...")
+        self.recording = False
         stream.stop_stream()
         stream.close()
-
+        
         audio.terminate()
 
         return frames
@@ -61,15 +65,23 @@ def transcribe() -> str:
     """Transcribes the audio file into text"""
     with open(filename, 'rb') as fp:
         transcript = openai.Audio.transcribe("whisper-1", fp)
-        return transcript['text']
+        return transcript['text'].strip()
 
 
-def prompt(text:str) -> str:
+def prompt(context:list) -> str:
     """Prompts the text-davinci-003 model for a response given the recorded text"""
-    p = "You are role-playing as an amazing assistant named Timmy. Your role is to try to provide accurate information in an entertaining way to anyone who may need assistance. You are always fairly easy going, and respond directly but yet still in a friendly manner. You were developed by Isaiah Carrington, and are powered by Open Ai.\n" + text
-    response = openai.Completion.create(model="text-davinci-003", prompt=p, temperature=1, max_tokens=1000)
+    content = "CONTEXT:\n"+ "\n".join(context[1:-1]) + "\nQUERY:\n" + context[-1]
+    print(content)
+    response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role":context[0],
+                "content":content,
+            }], 
+            temperature=1,
+        )
 
-    return response["choices"][0]["text"]
+    return response["choices"][0]["message"]['content'].replace("RESPONSE:", "")
 
 
 def tts(text:str):
@@ -92,4 +104,4 @@ def tts(text:str):
     engine.runAndWait()
 
 def clean_up_text_for_speech(text:str):
-    return text.replace("|", ".")
+    return text.replace("|", ".").replace("-", "")
